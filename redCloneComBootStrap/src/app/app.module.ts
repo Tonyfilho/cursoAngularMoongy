ebc721c8b90308f9a8350d9f171258d4f65f088c
unknown <oficinadosdrones@gmail.com>
2022-05-21 15:38:36 +0100

    criação do html

--- redCloneComBootStrap/src/app/app.module.ts
+++ redCloneComBootStrap/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FatherComponent } from './father/father.component';
+import { SonComponent } from './son/son.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FatherComponent,
+    SonComponent
    
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
+    FatherComponent,
+    SonComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
