b635deedcf471fd7775bd4db9114dfd8d219a9f4
unknown <oficinadosdrones@gmail.com>
2022-06-05 16:57:15 +0100

    modal do filho funcionando, carossel dentro do modal filho funcionando, falta polular os dados

--- bookList/src/app/forecast/forecast.component.ts
+++ bookList/src/app/forecast/forecast.component.ts
@@ -1,41 +1,34 @@
 import { FormControl } from '@angular/forms';
 import { ForecastingService } from './forecasting.service';
-import {
-  AfterViewInit,
-  Component,
-  OnInit  
-} from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { debounceTime, switchMap, EMPTY } from 'rxjs';
-
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.css'],
 })
-export class ForecastComponent implements OnInit, AfterViewInit {  
-  searchCity: FormControl = new FormControl();  
-  forecastWithSwitch = {}; 
-  displayStyle = "none";
+export class ForecastComponent implements OnDestroy, AfterViewInit {
+  searchCity: FormControl = new FormControl();
+  forecastWithSwitch = [{}];  
+  wheatherCity = '';
+  modalDisplayStyle = 'none';
+  openOrCloseModal = 'none';
   image = './assets/background/built_day.jpg';
   isOpenforecasting = false;
   constructor(private fs: ForecastingService) {
     /**usando o serviço sem SWITCHMAP somente o Subscribe para cada stream */
-    // this.searchCity.valueChanges.subscribe(values => {
-    //   fs.getWeather(values).subscribe(forecast => { this.forecastWithoutSwitch = forecast,  console.log(this.forecastWithoutSwitch)});
-    // });
-    this.searchCity.valueChanges
-      .pipe(debounceTime(1500))
-      .subscribe((values) => {    
-        fs.getWeather(values).subscribe({
-          next: erro => {},
-          error: () => { }
-        })        
-      }      
-      );        
-      
-    }
-    /** com SwitchMap, basta ter 1 subscrição para as 2 streams, o Switchmap pega os valores q estão na STREAM do FormControl
+    // this.searchCity.valueChanges
+    //   .pipe(debounceTime(1500))
+    //   .subscribe((values) => {
+    //     fs.getWeather(values).subscribe({
+    //       next: erro => {},
+    //       error: () => { }
+    //     })
+    //   }
+    //   );
+  }
+  /** com SwitchMap, basta ter 1 subscrição para as 2 streams, o Switchmap pega os valores q estão na STREAM do FormControl
    * e manda para dentro da Stream de TEMPO q temos no Serviço ForecastingService, fazendo somente uam Subscrição.
    */
   ngAfterViewInit(): void {
@@ -43,56 +36,52 @@ export class ForecastComponent implements OnInit, AfterViewInit {
       .pipe(
         debounceTime(1000),
         switchMap((city: any) => {
-          if(city) {
-            return this.fs.getWeather(city)
+          if (city) {
+            this.wheatherCity = city;
+            return this.fs.getWeather(city);
           }
-          
+
           return EMPTY;
         })
       )
       .subscribe({
         next: (sucess) => {
-          (this.forecastWithSwitch = sucess);
-          if(sucess.weather){
+          if (sucess.weather) {
+            this.forecastWithSwitch = [sucess, this.image];            
             return this.changeImageFromApi(sucess.weather[0]['main']);
+          } else {
+            return this.openModalError();
           }
-          else {
-           return this.openModalError();
-          }           
         },
         error: (err) => {
           console.error(`error no request: ${err}`);
         },
         complete: () => {},
-      });  
-   
+      });
   }
-
-  ngOnInit(): void {}
-
-  
 
   /**
    * Will open the modal error
    */
   openModalError() {
-    this.displayStyle = "block";
+    this.modalDisplayStyle = 'block';
     this.searchCity.setValue('');
     this.changeImageFromApi('Default');
+    this.ngOnDestroy();
   }
   /**
    * will close the modal error
    */
   closeModalError() {
-    this.displayStyle = "none";
+    this.modalDisplayStyle = 'none';
     this.searchCity.setValue('');
     this.changeImageFromApi('Default');
+    this.ngOnDestroy();
   }
- /**
-  * will change the background-image of the template
- */
-  changeImageFromApi(image: string){
-    console.log('weather: ', image);
+  /**
+   * will change the background-image of the template
+   */
+  changeImageFromApi(image: string) {
     switch (image) {
       case 'Clear':
         this.image = './assets/background/nuvens_sol.jpg';
@@ -109,15 +98,19 @@ export class ForecastComponent implements OnInit, AfterViewInit {
       case 'Default':
         this.image = './assets/background/built_day.jpg';
         break;
-    
+
       default:
         this.image = './assets/background/built_day.jpg';
         break;
     }
   }
 
-  openForecasting(){
+  openForecasting() {
     this.isOpenforecasting = !this.isOpenforecasting;
+    this.openOrCloseModal = 'block';
   }
 
+  ngOnDestroy(): void {
+    this.searchCity.valueChanges.subscribe(() => {}).unsubscribe();
+  }
 }
--- bookList/src/app/forecast/forecasting-city/forecasting-city.component.ts
+++ bookList/src/app/forecast/forecasting-city/forecasting-city.component.ts
@@ -1,15 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, Input, OnDestroy,  } from '@angular/core';
 
 @Component({
   selector: 'app-forecasting-city',
   templateUrl: './forecasting-city.component.html',
   styleUrls: ['./forecasting-city.component.css']
 })
-export class ForecastingCityComponent implements OnInit {
-  @Input() forecastWithSwitch = {}
-  constructor() { }
-
-  ngOnInit(): void {
+export class ForecastingCityComponent implements  AfterContentInit{
+  @Input() forecastWithSwitch: any = {}
+   openOrCloseModal = 'block'
+   wheatherCity = '';
+   image = ''
+  constructor() {
+    console.log('Forcaste no construtor, vem Undefined: ', this.forecastWithSwitch)
   }
-
+  /**Permite ter acesso aos dados depois q o conteudo foi iniciado, pois foi Recebido do componente PAI 
+   * que manda um OBSERVABLE q recebemos ele via INPUT()
+   */
+  ngAfterContentInit(): void {
+    console.log('Forcaste dentro do COntent: ', this.forecastWithSwitch);
+    this.wheatherCity = this.forecastWithSwitch[0]['name'];
+    this.image = this.forecastWithSwitch[1];
+  }
+  
+  
+  
+  /**
+   * Close the modal
+   */
+  closeModal(){
+    this.openOrCloseModal = 'none';
+    
+  }
+  
+  /**
+   * 
+   * @param text Format the 1º lettle of word to upcase()
+   * @returns 
+   */
+  formtTheFirstLetter(text : string) {
+    let loweredText = text?.toLowerCase();
+    let words = loweredText?.split(" ");
+    for (let a = 0; a < words?.length; a++) {
+      let w = words[a];
+      let firstLetter = w[0];        
+      w = firstLetter?.toUpperCase() + w.slice(1);
+      words[a] = w;
+    }
+    return words?.join(" ");
+  }
+  
+  
 }
