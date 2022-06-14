14cd279b36fd3f0b894cdd004491e92de84f2544
tonyfilho <oficinadosdrones@gmail.com>
2022-06-14 17:24:37 +0100

    criação do section template driven e seu css que falta terminar

--- digitalCV/src/app/forms/forms.module.ts
+++ digitalCV/src/app/forms/forms.module.ts
@@ -2,15 +2,22 @@ import { FormsComponent } from './forms.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ReactiveFormsComponent,
+    TemplateDrivenFormsComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {path: '', component: FormsComponent},
+      {path:'reactive', component: ReactiveFormsComponent},
+      {path:'driven', component: TemplateDrivenFormsComponent},
       
     ])
   ]
