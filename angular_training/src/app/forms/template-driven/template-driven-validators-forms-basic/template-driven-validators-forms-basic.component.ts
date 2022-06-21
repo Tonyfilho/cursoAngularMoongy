aea3734c1670d1ae9e0ac9d10f44e954daaedaf1
tonyfilho <oficinadosdrones@gmail.com>
2022-06-21 16:09:23 +0100

    It doing the validators

--- angular_training/src/app/forms/template-driven/template-driven-validators-forms-basic/template-driven-validators-forms-basic.component.ts
+++ angular_training/src/app/forms/template-driven/template-driven-validators-forms-basic/template-driven-validators-forms-basic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, AfterViewInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,18 +6,34 @@ import { NgForm } from '@angular/forms';
   templateUrl: './template-driven-validators-forms-basic.component.html',
   styleUrls: ['./template-driven-validators-forms-basic.component.css']
 })
-export class TemplateDrivenValidatorsFormsBasicComponent implements OnInit {
-
+export class TemplateDrivenValidatorsFormsBasicComponent implements OnInit, AfterViewInit {
+  hasErrorNome!: boolean;
   constructor() { }
+  ngAfterViewInit(): void {
+  
+  }
+  
 
   ngOnInit(): void {
   }
   submitForms(f: NgForm ) {
+    /**Aqui são as 3 formas de buscar o OBJETO Required dentro f.controls['nome']?.errors
+     * 1º atribuindo para var this.hasErrorNome = f.controls['nome'].errors?.['required'];
+     * 2º criando uma var que é um Objeto e atribuindo valor requiredSemDestruition = f?.controls['nome']?.errors as any 
+     * Obs: Temos q converter para ANY usando As ANY.
+     * 3º usando o destruinction const  { required } = f.controls['nome'].errors as any
+     * Obs: Temos q converter para ANY usando As ANY.
+    */
+    this.hasErrorNome = f.controls['nome'].errors?.['required'];
     const {name, address, email, ssn, passwordGroup: {password, confirmPassword} } = f.value ;
-    console.group();
-    console.log('Destructions', name, address, email, ssn, password, confirmPassword);
+    const  requiredSemDestruition = f?.controls['nome']?.errors as any;
+    const  { required } = f.controls['nome'].errors as any ;
+    console.log('Destructions', name, address, email, ssn, password, confirmPassword, required );
     console.groupEnd();
-    console.log('FORM',f);
+    console.log('required', requiredSemDestruition['required'], this.hasErrorNome);
+    console.log('Objeto_de_Validator',f.controls['nome']?.errors );
+    console.log('dirty',f.controls['nome'].dirty);
+    console.log('pristine',f.controls['nome'].pristine);
     console.log(f.value);
     f.reset();
   }
