98557d352e25f570cb9d1b61cd624939bd815451
tonyfilho <oficinadosdrones@gmail.com>
2022-07-07 20:08:12 +0100

    created sign-up and css and template

--- angular_training/src/app/login/sign-up/sign-up.component.ts
+++ angular_training/src/app/login/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { AuthenticationService } from 'src/app/_services/authentication.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router, private auth: AuthenticationService, private tost:  HotToastService) {
+    this.loginForm = fb.group({
+      nome: ['', {validators: [Validators.required,Validators.minLength(3)], updateOn: 'blur'}],
+      email: ['', { validators: [Validators.required, Validators.email], updateOn: 'blur' }],
+      passwordGroup : fb.group({
+         password: ['', [Validators.required]],
+         pConfirm: ['',  [Validators.required]], updateOn: 'blur', validators: [],
+      })
+
+
+
+    })
+  }
 
   ngOnInit(): void {
+    this.loginForm.controls['email'].getError('required');
+    this.loginForm.controls['email'].dirty;
   }
+  close() {
+    this.router.navigateByUrl('/home');
+  }
+  ngOnDestroy(): void {
+    this.close;
+  }
+
+  /**1º Ver se  o form é INvalido, caso seja ja retornar */
+  submit() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+
+    const { email, password } = this.loginForm.value
+   this.auth.login(email, password).pipe(
+    this.tost.observe({
+      success: 'Logged in Sucessfully',
+      loading: 'Logging in ..',
+      error: 'There was as error'
+    })
+   ).subscribe({
+    next: () => {this.router.navigate(['/home'])},
+    error: () => {this.resertForm}
+
+    })
+  }
+
+   get resertForm() {
+    return this.loginForm.reset();
+   }
 
 }
