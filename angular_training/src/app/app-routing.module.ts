253ba958dedfe55bc7f0417b1b1d64c16bb2cdda
tonyfilho <oficinadosdrones@gmail.com>
2022-07-08 08:49:29 +0100

    ANGULARFIRE`s canActivate with protection routers is working

--- angular_training/src/app/app-routing.module.ts
+++ angular_training/src/app/app-routing.module.ts
@@ -8,32 +8,48 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login/login.component';
 import { SignUpComponent } from './login/sign-up/sign-up.component';
 
+
+
+/**São importações Auth do angularFire, é uma forma mais fácil de proteger
+ * rotas quando usamos o firebase, Criando PIPES locais
+ */
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo }  from '@angular/fire/auth-guard'
+/**Usuario não Autorizado ou não logado Redireciona para Login*/
+const redirectToLogin = () => redirectUnauthorizedTo(['/login']);
+/**Usuario Autorizado ou  logado, Redireciona para Paginas */
+const redirectToHome = () => redirectLoggedInTo(['/home']);
+
+
+
 const routes: Routes = [
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'books', component: BooksComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  /**Nos Outros LINKS, caso receba um FALSE por falta de autorização, o FireBase Authorization,
+   *  a pagina será redirecinada para login*/
+  { path: 'home', component: HomeComponent, ...canActivate(redirectToLogin)},
+  { path: 'books', component: BooksComponent ,...canActivate(redirectToLogin) },
   {
     path: 'books/:id',
-    component: BookComponent,
+    component: BookComponent,...canActivate(redirectToLogin)
   },
   {
     path: 'forecasting',
-    component: ForecastComponent,
+    component: ForecastComponent,...canActivate(redirectToLogin)
   },
+  /**Nos Link LOGIN e LOGIN/SIGN-UP, caso receba um TRUE do FireBase Authorization, a pagina será redirecinada para home*/
   {
     path: 'login',
-    component: LoginComponent,
+    component: LoginComponent, ...canActivate(redirectToHome)
   },
   {
     path: 'login/sign-up',
-    component: SignUpComponent,
+    component: SignUpComponent, ...canActivate(redirectToHome)
   },
   /*************************************************Carregando LAZYLOAD ROUTE*/
-  { path: 'forms', loadChildren: () => import('./forms/forms-local.module').then(module => module.FormsLocalModule)},
+  { path: 'forms', loadChildren: () => import('./forms/forms-local.module').then(module => module.FormsLocalModule), ...canActivate(redirectToLogin)},
   { path: 'about', loadChildren: () => import('./about/about.module').then(module => module.AboutModule)},
   {
     path: '**',
-    component: NotFoundComponent,
+    component: NotFoundComponent ,...canActivate(redirectToLogin)
   },
 ];
 
