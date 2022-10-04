ccc0e6988483edfe7d092932406d572c61dbe950
tonyfilho <oficinadosdrones@gmail.com>
2022-10-04 14:14:57 +0100

    on-init-and-on-changed.component

--- angular_training/src/app/lifecycle/lifecycle.module.ts
+++ angular_training/src/app/lifecycle/lifecycle.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import { LifecycleComponent } from './lifecycle.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { OnInitAndOnChangedComponent } from './on-init-and-on-changed/on-init-and-on-changed.component';
 
 
 
 @NgModule({
   declarations: [
-    LifecycleComponent
+    LifecycleComponent,
+    OnInitAndOnChangedComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild([])
+    RouterModule.forChild([
+      {path: '' ,component:LifecycleComponent},
+      {path: 'onInitOnChange' ,component:OnInitAndOnChangedComponent}
+    ])
   ]
 })
 export class LifecycleModule { }
---
+++ angular_training/src/app/lifecycle/on-init-and-on-changed/on-init-and-on-changed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-on-init-and-on-changed',
+  templateUrl: './on-init-and-on-changed.component.html',
+  styleUrls: ['./on-init-and-on-changed.component.css']
+})
+export class OnInitAndOnChangedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
