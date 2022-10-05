fb103a52be3fb4ac440ad0ff0f03d4d090489661
tonyfilho <oficinadosdrones@gmail.com>
2022-10-05 11:28:16 +0100

    fixing route

--- angular_training/src/app/gamer-control/odd/odd.component.ts
+++ angular_training/src/app/gamer-control/odd/odd.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./odd.component.css']
 })
 export class OddComponent implements OnInit {
-
+  @Output("gamer") gamer: EventEmitter<number> = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  startGamer() {}
+
+  stopGamer(){}
 
 }
