a0eb76f13a6c429fa9f18dd5da5f3558f8fbcc2b
tonyfilho <oficinadosdrones@gmail.com>
2022-10-05 11:05:28 +0100

    fixing names

--- angular_training/src/app/gamer-control/gamer.module.ts
+++ angular_training/src/app/gamer-control/gamer.module.ts
@@ -3,21 +3,21 @@ import { CommonModule } from '@angular/common';
 import { GamerComponent } from './gamer.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { OnInitAndOnChangedComponent } from './on-init-and-on-changed/on-init-and-on-changed.component';
+import { OddComponent } from './odd/odd.component';
 
 
 
 @NgModule({
   declarations: [
     GamerComponent,
-    OnInitAndOnChangedComponent
+    OddComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild([
       {path: '' ,component:GamerComponent},
-      {path: 'gamerControl' ,component:OnInitAndOnChangedComponent}
+      {path: 'gamerControl' ,component:OddComponent}
     ])
   ]
 })
