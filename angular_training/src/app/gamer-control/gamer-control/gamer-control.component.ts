ab8eedc31b5bf11427c3540054db2dff7ffde69c
tonyfilho <oficinadosdrones@gmail.com>
2022-10-05 11:16:46 +0100

    starting

---
+++ angular_training/src/app/gamer-control/gamer-control/gamer-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gamer-control',
+  templateUrl: './gamer-control.component.html',
+  styleUrls: ['./gamer-control.component.css']
+})
+export class GamerControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular_training/src/app/gamer-control/gamer.module.ts
+++ angular_training/src/app/gamer-control/gamer.module.ts
@@ -4,20 +4,24 @@ import { GamerComponent } from './gamer.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
+import { GamerControlComponent } from './gamer-control/gamer-control.component';
 
 
 
 @NgModule({
   declarations: [
     GamerComponent,
-    OddComponent
+    OddComponent,
+    EvenComponent,
+    GamerControlComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild([
       {path: '' ,component:GamerComponent},
-      {path: 'gamerControl' ,component:OddComponent}
+      {path: 'gamerControl' ,component:GamerControlComponent}
     ])
   ]
 })
