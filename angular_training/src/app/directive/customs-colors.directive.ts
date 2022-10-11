0e874232af6b7292ffa39a8804bdf2a8f8d92762
tonyfilho <oficinadosdrones@gmail.com>
2022-10-11 08:36:47 +0100

    class 99 Binding to directive properties

--- angular_training/src/app/directive/customs-colors.directive.ts
+++ angular_training/src/app/directive/customs-colors.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCustomsColors]'
 })
 export class CustomsColorsDirective {
 
-  constructor() { }
+ /**Customizaremos a cor  do backGround */
+// @Input("defaultColors") defaultColors: string = "";
+@Input("customsColors") customsColors: string = "Blue";
 
+/** 1º Adcionaremos a Anotação @Hostlistener , permite usar metodos que temos no teclado ou no mouse */
+/** 2º Adcionaremos a Anotação @Hostbinding , permite manipular o DOM sem o Renderer2 */
+/** 3º com @Hostbinding criaremos a Propriedade de cor. Obs: Poderia ser qualquer uma  */
+/** 4ª Dentro do @Hostdbing temos que passar Qual a propriedade do ELEMENTO queremos fazer o BINDING, neste caso style.backgroundColor
+ * Obs: Usar o camelCase, pois oDOM não reconhece "-" background-color.
+ */
+ @HostBinding("style.backgroundColor")  localBackGroundColor: string = "";
+
+ constructor(private elRel:ElementRef) {
+}
+ngOnInit(): void {
+
+}
+
+/** 5º Sem precisa do mais  renderer2: Renderer2, podemos setar os valores usanso o @Input customsColors */
+
+
+
+/** Quando passar o mouse sobre a lista, mudaremos a cor da mesma */
+@HostListener('mouseenter') mouseOver(evendData: Event) {
+  this.localBackGroundColor = this.customsColors === "Blue" ? "Blue": this.customsColors;
+
+}
+@HostListener('mouseleave') mouseleave(evendData: Event) {
+  this.localBackGroundColor = "";
+
+}
 }
