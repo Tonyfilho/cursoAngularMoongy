af43a0070ad37d2f85b913ebfcd4b9072af62b83
tonyfilho <oficinadosdrones@gmail.com>
2022-10-31 09:54:51 +0000

    need to fixing query-parameters-id.component.css

--- angular_training/src/app/routes/query-parameters/query-parameters-id/query-parameters-id.component.ts
+++ angular_training/src/app/routes/query-parameters/query-parameters-id/query-parameters-id.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Book } from 'src/app/_models/book.model';
+import { RoutesService } from '../../routes.service';
 
 @Component({
   selector: 'app-query-parameters-id',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./query-parameters-id.component.css']
 })
 export class QueryParametersIdComponent implements OnInit {
+  book?: Observable<Book | undefined>;
+  localId: number | any  = 0;
+  localName?: string;
 
-  constructor() { }
+  constructor(private routesService:RoutesService, private route: ActivatedRoute) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    /**Using Observable */
+    this.route.params.subscribe((data: Params) =>  {this.localId = data["id"]} );
+
+   this.book = this.routesService.getBookById(parseInt(this.localId));
+
+  }
 }
