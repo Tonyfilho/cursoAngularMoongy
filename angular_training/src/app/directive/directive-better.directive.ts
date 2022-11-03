4f55640891d45bc6cc9cf3b0c54e2f328931fadb
tonyfilho <oficinadosdrones@gmail.com>
2022-11-03 14:21:37 +0000

    advance-directive.component done

--- angular_training/src/app/directive/directive-better.directive.ts
+++ angular_training/src/app/directive/directive-better.directive.ts
@@ -4,11 +4,13 @@ import { Directive, OnInit, Renderer2, ElementRef, Input } from '@angular/core';
   selector: '[appDirectiveBetter]'
 })
 export class DirectiveBetterDirective implements OnInit {
-  @Input("colorChange") colorChange!:string;
-  constructor(private elRef: ElementRef,private renderer: Renderer2) { }
-/**Com o Renderer2 já traz varios metodos de redenrizar o DOM de forma mais segura, ao invez de passar o ElementRef direto, passaremos o RENDERE2 no lugar
- * mas aqui injetaremos o ElementoRef conforme o Exemplo Abaixo. Pois é ele que vai renderizar atravez do Rendered.
- */
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {/*
+  *  Com o Renderer2 já traz varios metodos de redenrizar o DOM de forma mais segura, ao invez de passar o ElementRef direto, passaremos o RENDERE2 no lugar
+  *  mas aqui injetaremos o ElementoRef conforme o Exemplo Abaixo. Pois é ele que vai renderizar atravez do Rendered.
+  */
+
+  }
+
   ngOnInit(): void {
     this.renderer.setStyle(this.elRef.nativeElement, "background-color", "blue"); /**backgroundColor */
   }
