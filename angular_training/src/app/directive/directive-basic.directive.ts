ffd2f732b218879e91e1877bb3e97bac15663c22
tonyfilho <oficinadosdrones@gmail.com>
2022-11-03 17:52:19 +0000

    basic-high-light-directive.component

--- angular_training/src/app/directive/directive-basic.directive.ts
+++ angular_training/src/app/directive/directive-basic.directive.ts
@@ -4,15 +4,13 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
   selector: '[appDirectiveBasic]'
 })
 export class DirectiveBasic implements OnInit {
-  red!: string;
-  /**Esta forma não é melhor  conceito de mudar o DOM, não é muito indicado pela comunidade Angular, mas funciona. */
-  /**O ElementoRef é o mesmo usado com o ViewChild e ViewContent */
-  constructor(private elementRef: ElementRef) {
+
+  constructor(private elementRef: ElementRef) {/**Esta forma não é melhor  conceito de mudar o DOM, não é muito indicado pela comunidade Angular, mas funciona.
+    O ElementoRef é o mesmo usado com o ViewChild e ViewContent */
   }
 
   ngOnInit(): void {
-   this.red = this.elementRef.nativeElement.style.backgroundColor="red";
-    // this.elementRef.nativeElement.style.background = 'linear-gradient(68.15deg, #2e2e30 16.62%, #040013 85.61%)';
+    this.elementRef.nativeElement.style.backgroundColor = "red";
   }
 
 }
