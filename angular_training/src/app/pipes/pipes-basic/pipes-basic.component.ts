d630d53db54dc63747d91083fa4ade4b0f89a95b
tonyfilho <oficinadosdrones@gmail.com>
2022-12-06 08:40:55 +0000

    create pipes-basic.component

---
+++ angular_training/src/app/pipes/pipes-basic/pipes-basic.component.ts
@@ -0,0 +1,22 @@
+import { PipesService } from './../pipes.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes-basic',
+  templateUrl: './pipes-basic.component.html',
+  styleUrls: ['./pipes-basic.component.css']
+})
+export class PipesBasicComponent implements OnInit {
+  servers: any[] = [];
+  constructor(private pipesService: PipesService) {
+    this.servers = [... pipesService.serversDummy];
+   }
+
+  ngOnInit(): void {
+  }
+
+  getStatusClasses(server: { instanceType: string; name: string; status: string; started: Date; }) {
+    return this.pipesService.getStatusClasses(server)
+  }
+
+}
--- angular_training/src/app/pipes/pipes.module.ts
+++ angular_training/src/app/pipes/pipes.module.ts
@@ -2,17 +2,20 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PipesComponent } from './pipes.component';
+import { PipesBasicComponent } from './pipes-basic/pipes-basic.component';
 
 
 
 @NgModule({
   declarations: [
     PipesComponent,
+    PipesBasicComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path:'', component:PipesComponent }
+      {path:'', component:PipesComponent },
+      {path:'pipes-basics', component:PipesBasicComponent }
   ])
   ]
 })
