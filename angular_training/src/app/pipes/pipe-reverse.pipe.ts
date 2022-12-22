4275ded38e5009dc9d8be95bf08baea2cc1d6a10
tonyfilho <oficinadosdrones@gmail.com>
2022-12-22 13:53:04 +0000

    need to fix sortBy

--- angular_training/src/app/pipes/pipe-reverse.pipe.ts
+++ angular_training/src/app/pipes/pipe-reverse.pipe.ts
@@ -4,9 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'pipeReverse'
 })
 export class PipeReversePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+ /**Deixaremos os operadore do JS fazer tudo,
+  * 1º SPLIT(''): Dividiremos a string e separeremos por espaço.
+  * 2º REVERSE(): Reverteremos a ordem da string
+  * 3º JOIN(): Juntaremos a nova String  */
+  transform(value: string): unknown {
+    return value.split('').reverse().join('');
   }
 
 }
--- angular_training/src/app/pipes/pipe-sort-by.pipe.ts
+++ angular_training/src/app/pipes/pipe-sort-by.pipe.ts
@@ -4,9 +4,28 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'pipeSortBy'
 })
 export class PipeSortByPipe implements PipeTransform {
+  /** UNICODE EX:
+   * var scores = [1, 10, 2, 21];
+   *scores.sort(); // [1, 10, 2, 21]
+   *Observe que 10 vem antes do 2,
+   *porque '10' vem antes de '2' em ponto de código Unicode.
+   */
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  /**Deixaremos os operadore do JS fazer tudo,
+ * 1º SORT(''): Organizaremos o Array de string, ms se deixamos somente o SORT()
+ *  sem passar um função que organiza ele usara o padrão UNICODE
+ * Com isto temos que somente checar se o que vem no index é maior do que proximo index .
+ * 2º Os Argumento são passado automaticamente pelo JS, não precisamos passar
+ */
+
+  transform(value: any[], proName: string ): unknown  {
+    return value.sort((maior: string, menor: string) => {
+      if(maior[proName] > menor[proName] )
+      return 1;
+      else {
+        return -1;
+      }
+    });
   }
 
 }
