e9f33d7ccd98188ba1760ee143389803c2639b15
tonyfilho <oficinadosdrones@gmail.com>
2022-12-27 08:15:03 +0000

    create post.component needs to fix the css

--- angular_training/src/app/http-request/http-resquest.module.ts
+++ angular_training/src/app/http-request/http-resquest.module.ts
@@ -1,7 +1,9 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpRequestComponent } from './http-request.component';
 import { PostComponent } from './post/post.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +13,12 @@ import { PostComponent } from './post/post.component';
     PostComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      {path:'', component: HttpRequestComponent},
+      {path:'post', component: PostComponent},
+    ])
   ]
 })
-export class HttpResquestAndObservablesModule { }
+export class HttpResquestModule { }
