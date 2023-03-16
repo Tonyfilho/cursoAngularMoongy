6efd6155903378a59963e74924b78099aa7a7b51
tonyfilho <oficinadosdrones@gmail.com>
2023-03-16 07:36:26 +0000

    create class RXJS e observables-e-rxjs.component

---
+++ angular_training/src/app/observables-e-RXJS/observables-e-rxjs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-observables-e-rxjs',
+  templateUrl: './observables-e-rxjs.component.html',
+  styleUrls: ['./observables-e-rxjs.component.css']
+})
+export class ObservablesERXJSComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular_training/src/app/observables-e-RXJS/observables.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ObservablesERXJSComponent } from './observables-e-rxjs.component';
+
+
+
+
+@NgModule({
+  declarations: [
+
+
+    ObservablesERXJSComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ObservablesModule { }
