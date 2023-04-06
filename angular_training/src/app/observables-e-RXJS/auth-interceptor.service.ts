d14a4c2f2b49edaf2762d89ba3a3c06ab241dbcf
tonyfilho <oficinadosdrones@gmail.com>
2023-04-06 09:01:23 +0100

    fim do resquest modulo

--- angular_training/src/app/observables-e-RXJS/auth-interceptor.service.ts
+++ angular_training/src/app/observables-e-RXJS/auth-interceptor.service.ts
@@ -7,14 +7,24 @@ import { Observable } from 'rxjs';
 })
 
 export class AuthInterceptorService implements HttpInterceptor {
-
+   /**
+    * 1ª o REQ, ou request é imutable, ou seja caso queiramos modifica lo temos que clonar,
+    * 2º podemos clonar URL
+    * 3º podemos clonar o HEADERS
+    * 4º podemos  params
+    */
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log("Request is on its way");
-    return next.handle(req);
+    console.log('console req.url',req.url);
+    const modifieldResquest = req.clone({
+      headers: req.headers.append('MyAuth', 'component auth-interceptor.services')
+    })
+   // return next.handle(req);
+    return next.handle(modifieldResquest);
   }
 
 
 
-  
+
 }
